27096ecea6e1e91dda333f1884bf0c4c0405e2ec
liamoneill <liam.mu.oneill@gmail.com>
2015-08-01 14:48:12 +1200

    Added weather api Added weather api Changed api routes to sperate weather and soon to be powershop apis

--- api/weather.js
+++ api/weather.js
@@ -1,11 +1,45 @@
+//var request = require('request');
 var weather = require('openweathermap');
 
 // set defaults
-weather.defaults({units:'metric', lang:'en', mode:'json'});
+function formName(cityName){
+	return cityName + ',nz';
+}
+
+function formDate(jsDate){
+	return Date.parse(jsDate).getTime() / 1000;
+}
+
+function defaultOptions(cityName){
+	return { 'q': formName(cityName), 'type': 'daily', 'units': 'metric' };
+}
+
+function getHistory(cityName, startDate, endDate, callback){
+	var options = defaultOptions(cityName);
+	if(startDate){
+		options['start'] = formDate(startDate);
+	}
+	if(endDate){
+		options['end'] = formDate(endDate);
+	}
+	weather.history(options, function(err, result){
+		callback(err, result);
+	});
+}
+
+function getForecast(cityName, count, callback){
+	var options = defaultOptions(cityName);
+	if(count){
+		options['ctn'] = count;
+	}
+	weather.forecast(options, function(err, result){
+		callback(err, result);
+	});
+}
 
 var out = {
 	getHistory: getHistory,
+	getForecast: getForecast
 
 };
-
 module.exports = out;
