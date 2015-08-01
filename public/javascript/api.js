0f6e21eafc7f44c47d42f554b5682de614554ec0
Daniel Braithwaite <danielbraithwt@gmail.com>
2015-08-02 09:40:21 +1200

    Accessing Back End API JS Started on the js to acces the back end API, however its not completed yet as we still havent sorted out the oauth

--- public/javascript/api.js
+++ public/javascript/api.js
@@ -1,20 +1,43 @@
-(function() {
-	'use strict'
-	
-	var keys = undefined;
-	
-	apiFunctions = {};
-	
-	apiFunctions.getData = function() {
+
+function getData(callback) {
+
+	// Make sure that we have the key, otherwise return
+	// undefined
+//	if (key === undefined) {
+//		return undefined;	
+//	}
+
+	// Access the api and run the callback with the data
+	// First get the powershop data and then get the wether
+	$.get("/api/powershop/usage_data", function(power, status) {
 		
-		// Make sure that we have the key, otherwise return
-		// undefined
-		if (key === undefined) {
-			return undefined;	
-		}
+		// Get just the power usage
+		var powerUsage = power.result.data;//[];
+		//for (var i = 0; i < power.result.length; i++) {
+		//	powerUsage.push(power.result[i]);	
+		//}
 		
-		// Access the api and return a call back
-	}
-	
-	return apiFunctions;
-})()
+		// Only temprarly hard coded
+		$.get("/api/weather/history/wellington", function(wether, status) {
+
+			// Get a list of dates
+			var dates = [];
+			for (var i = 0; i < wether.length; i++) {
+				dates.push(wether[i].date)	
+			}
+			
+			var maxTemp = [];
+			for (var i = 0; i < wether.length; i++) {
+				maxTemp.push(wether[i].max);	
+			}
+
+			graphData = {
+				dates: dates,
+				power_usage: powerUsage,
+				wether: wether,
+			}
+
+			callback(graphData);
+		});
+	});
+}
