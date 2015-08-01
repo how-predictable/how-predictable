9a6b8a038a10c0c6f6350d1face7eb8f1b229a01
Daniel Braithwaite <danielbraithwt@gmail.com>
2015-08-02 10:38:41 +1200

    Fixed Fake Powershop Data Fixed how the fake data was stored so it matches the format of the real data

--- routes/powershopApi.js
+++ routes/powershopApi.js
@@ -3,57 +3,60 @@ var router = express.Router();
 var powershop = require('../api/powershop');
 
 router.get('/oauth', function(req, res, e) {
-    res.send(powershop.auth());
+	res.send(powershop.auth());
 });
 
 router.get('/usage_data', function(req, res, e) {
-    res.send({
-        data: [
-          1234,
-          23423,
-          1212,
-          234,
-          345345,
-          123423,
-          2342,
-          234,
-          23423,
-          234234,
-          1234,
-          23423,
-          1212,
-          234,
-          345345,
-          123423,
-          2342,
-          234,
-          23423,
-          234234,
-          1234,
-          23423,
-          1212,
-          234,
-          345345,
-          123423,
-          2342,
-          234,
-          23423,
-          234234,
-          12343,
-          7834,
-          7345
-        ]
-    });
+	res.send({
+		verson: 2.0,
+		result: {
+			data: [
+				1234,
+				23423,
+				1212,
+				234,
+				345345,
+				123423,
+				2342,
+				234,
+				23423,
+				234234,
+				1234,
+				23423,
+				1212,
+				234,
+				345345,
+				123423,
+				2342,
+				234,
+				23423,
+				234234,
+				1234,
+				23423,
+				1212,
+				234,
+				345345,
+				123423,
+				2342,
+				234,
+				23423,
+				234234,
+				12343,
+				7834,
+				7345
+			]
+		},
+	});
 });
 
 router.get('/auth', function(req, res, e) {
-    powershop.auth(function(err, url) {
-        if (!err) {
-          res.redirect(url);
-        } else {
-          res.redirect("/"); // FIXME: Add error message to user
-        }
-    });
+	powershop.auth(function(err, url) {
+		if (!err) {
+			res.redirect(url);
+		} else {
+			res.redirect("/"); // FIXME: Add error message to user
+		}
+	});
 });
 
 module.exports = router;
