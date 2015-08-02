6ffcd9fc7c399e543a6e3fe7d4fdfa778cac6b5d
liamoneill <liam.mu.oneill@gmail.com>
2015-08-02 16:14:12 +1200

    Added unit.js

--- test/api/historyTest.js
+++ test/api/historyTest.js
@@ -3,11 +3,12 @@ var weather = require('../../api/weather');
 var exports = module.exports;
 
 exports.testPropeties = function(test){
-    weather.getHistory(null, function(){
+    weather.getHistory(null, function(err, history){
         test.assert(history.length == 32);
         var element = history[0];
         test.assert(element.hasOwnProperty('date'));
         test.assert(element.hasOwnProperty('maxTemp'));
         test.assert(element.hasOwnProperty('minTemp'));
+        test.done();
     });
 };
