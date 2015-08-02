29ac960d1d481deb0a7b3b4b408acb6d81947b31
David Barnett <david@barnett.net.nz>
2015-08-02 13:22:58 +1200

    removal of hard coded keys

--- api/powershop.js
+++ api/powershop.js
@@ -3,15 +3,16 @@ var OAuth = require('oauth');
 
 var api = "https://qa.test.powershop.co.nz/external_api";
 var oauthTokenSecert;
+var oauthAccessSecert;
 
 var oauth = new OAuth.OAuth(
-  'https://qa.test.powershop.co.nz/external_api/oauth/request_token',
-  'https://qa.test.powershop.co.nz/external_api/oauth/access_token',
-  'eb1814912f40af64556a3797b6d116df', // test user token
-  's65zImE9Rk1hMfjaEMfTOnTj3ilHs57v', // test user secret
-  '1.0A',
-  'http://localhost:3000/api/powershop/token', // FIXME: real url plz
-  'HMAC-SHA1'
+	'https://qa.test.powershop.co.nz/external_api/oauth/request_token',
+	'https://qa.test.powershop.co.nz/external_api/oauth/access_token',
+	'eb1814912f40af64556a3797b6d116df', // test user token
+	's65zImE9Rk1hMfjaEMfTOnTj3ilHs57v', // test user secret
+	'1.0A',
+	'http://localhost:3000/api/powershop/token', // FIXME: real url plz
+	'HMAC-SHA1'
 );
 
 /**
@@ -19,88 +20,91 @@ var oauth = new OAuth.OAuth(
  * using the wonders of OAuth!
  */
 function auth(callback) {
-  oauth.getOAuthRequestToken(function(error, oauth_token, oauth_token_secret, results) {
-      if (error) {
-        console.log('error: ' + JSON.stringify(error));
-        callback(error, null);
-      } else {
-        console.log('oauth_token: ' + oauth_token);
-        console.log('oauth_token_secret: ' + oauth_token_secret);
-        oauthTokenSecert = oauth_token_secret;
-        console.log('requestoken results: ' + JSON.stringify(results));
-        console.log("Requesting access token");
-        callback(null, "https://qa.test.powershop.co.nz/external_api/oauth/authorize?oauth_token=" + oauth_token);
-      }
-  });
+	oauth.getOAuthRequestToken(function(error, oauth_token, oauth_token_secret, results) {
+			if (error) {
+				console.log('error: ' + JSON.stringify(error));
+				callback(error, null);
+			} else {
+				console.log('oauth_token: ' + oauth_token);
+				console.log('oauth_token_secret: ' + oauth_token_secret);
+				oauthTokenSecert = oauth_token_secret;
+				console.log('requestoken results: ' + JSON.stringify(results));
+				console.log("Requesting access token");
+				callback(null, "https://qa.test.powershop.co.nz/external_api/oauth/authorize?oauth_token=" + oauth_token);
+			}
+	});
 }
 
 function getToken(oauthToken, oauthVerifier, callback) {
-  oauth.getOAuthAccessToken(oauthToken, oauthTokenSecert,  oauthVerifier,
-    function (err, oauth_access_token, oauth_access_token_secret, results) {
-      if (!err) {
-        console.log("OAuth Access token: " +oauth_access_token);
-        console.log("OAuth Access secert: " +oauth_access_token_secret);
-        callback(null, '/dash.html' + "?token=" + oauth_access_token);
-      } else {
-        console.log(err);
-        callback(err, null);
-      }
-  });
+	oauth.getOAuthAccessToken(oauthToken, oauthTokenSecert,  oauthVerifier,
+		function (err, oauth_access_token, oauth_access_token_secret, results) {
+			if (!err) {
+				console.log("OAuth Access token: " +oauth_access_token);
+				console.log("OAuth Access secert: " +oauth_access_token_secret);
+				oauthAccessSecert = oauth_access_token_secret;
+				callback(null, '/dash.html' + "?token=" + oauth_access_token);
+			} else {
+				console.log(err);
+				callback(err, null);
+			}
+	});
 }
 
-function accounts(accessToken, accessTokenSecret, callback) {
-  oauth.get(
-    api + "/v2/accounts.js" ,
-    accessToken,
-    accessTokenSecret,
-    function(err, data, res){
-      if (!err) {
-        callback(null, data);
-      } else {
-        callback(err, null);
-      }
-    }
-  );
+function accounts(accessToken, callback) {
+	oauth.get(
+		api + "/v2/accounts.js" ,
+		accessToken,
+		oauthAccessSecert,
+		function(err, data, res){
+			if (!err) {
+				callback(null, data);
+			} else {
+				callback(err, null);
+			}
+		}
+	);
 }
 
-function getConnectionNumber(accessToken, accessTokenSecert, callback) {
-  accounts(accessToken, accessTokenSecert, function(err, data) {
-    if (!err) {
-      console.log(data);
-      callback(null, JSON.parse(data).result.accounts[0].properties[0].connection_number);
-    } else {
-      callback(err, null);
-    }
-  });
+function getConnectionNumber(accessToken, callback) {
+	accounts(accessToken, function(err, data) {
+			if (!err) {
+				callback(null, JSON.parse(data).result.accounts[0].properties[0].connection_number);
+			} else {
+				console.log(err);
+				callback(err, null);
+			}
+	});
 }
 
-function usageData(startDate, endData, consumerId,
-  accessToken, accessTokenSecret, callback) {
-  getConnectionNumber(accessToken, accessTokenSecret, function(err, data){
-      if (!err) {
-        oauth.get(
-          api + "/v1/usage_data.js?start_date=2015-01-01&end_date=2015-02-02&icp_number=" + data,
-          accessToken,
-          accessTokenSecret,
-          function(err, data, res){
-            if (!err) {
-              callback(null, data);
-            } else {
-              console.log(err);
-              callback(err, null);
-            }
-          }
-        );
-      } else {
-        callback(err, null);
-      }
+function usageData(startDate, endData,
+	accessToken, callback) {
+	getConnectionNumber(accessToken, function(err, data){
+			console.log(oauthAccessSecert);
+			if (!err) {
+				oauth.get(
+					api + "/v1/usage_data.js?start_date=2015-01-01&end_date=2015-02-02&icp_number=" + data,
+					accessToken,
+					oauthAccessSecert,
+					function(err, data, res){
+						if (!err) {
+							callback(null, data);
+						} else {
+							console.log(err);
+							callback(err, null);
+						}
+					}
+				);
+			} else {
+				console.log(err);
+				callback(err, null);
+			}
 
-  });
+	});
 }
 
 module.exports = {
-  auth: auth,
-  usageData: usageData,
-  accounts: accounts,
-  getToken: getToken,
+	auth: auth,
+	usageData: usageData,
+	accounts: accounts,
+	getToken: getToken,
 }
--- routes/powershopApi.js
+++ routes/powershopApi.js
@@ -1,66 +1,64 @@
 /**
-* Module for routing http://{domain name here}/api/powershop/... HTTP requests.
-*/
+ * Module for routing http://{domain name here}/api/powershop/... HTTP requests.
+ */
 var express = require('express');
 var router = express.Router();
 var powershop = require('../api/powershop');
 
 router.get('/oauth', function(req, res, e) {
-    res.send(powershop.auth());
+	res.send(powershop.auth());
 });
 
 router.get('/usage_data', function(req, res, e) {
-    var customerId;
-    var startDate;
-    var endDate;
-    powershop.usageData("","",customerId,
-      "95a8ecc99d688753dc2179929e1abc84",
-      "923d222c208c6094ed4941091bb6e170",
-      function(err, data) {
-        if (!err) {
-          res.send(data);
-        } else {
-          res.send(err);
-        }
-    });
+	var startDate = ""; // FIXME: use start & end dates
+	var endDate = "";
+
+	powershop.usageData(startDate, endDate,
+		req.query.token,
+		function(err, data) {
+			if (!err) {
+				res.send(data);
+			} else {
+				res.send(err);
+			}
+	});
 });
 
 router.get('/auth', function(req, res, e) {
-    powershop.auth(function(err, url) {
-        if (!err) {
-          res.redirect(url);
-        } else {
-          res.redirect("/"); // FIXME: Add error message to user
-        }
-    });
+	powershop.auth(function(err, url) {
+		if (!err) {
+			res.redirect(url);
+		} else {
+			res.redirect("/"); // FIXME: Add error message to user
+		}
+	});
 });
 
 router.get('/token', function(req, res, e) {
-    powershop.getToken(
-      req.query.oauth_token,
-      req.query.oauth_verifier,
-      function(err, url) {
-        if (!err) {
-          res.redirect(url);
-        } else {
-          res.redirect("/"); // FIXME: Add error message to user
-        }
-    });
+	powershop.getToken(
+		req.query.oauth_token,
+		req.query.oauth_verifier,
+		function(err, url) {
+			if (!err) {
+				res.redirect(url);
+			} else {
+				res.redirect("/"); // FIXME: Add error message to user
+			}
+	});
 });
 
 
 router.get('/accounts', function(req, res, e) {
-    powershop.accounts(
-      "95a8ecc99d688753dc2179929e1abc84",
-      "923d222c208c6094ed4941091bb6e170",
-      function(err, url) {
-        if (!err) {
-          res.send(url);
-        } else {
-          console.log(err);
-          res.redirect("/"); // FIXME: Add error message to user
-        }
-    });
+	powershop.accounts(
+		req.query.token,
+		function(err, url) {
+			if (!err) {
+				res.send(url);
+			} else {
+				console.log(err);
+				res.redirect("/"); // FIXME: Add error message to user
+			}
+	});
 });
 
 module.exports = router;
