dbbfb0857246fa521f9a9fe70994d1b02e338bbc
Daniel Braithwaite <danielbraithwt@gmail.com>
2015-08-02 12:41:13 +1200

    Dash Graph Styling Changed the styling of the graph on the dash board page to use more apropraite colors

--- public/javascript/dash.js
+++ public/javascript/dash.js
@@ -4,9 +4,9 @@ function drawUsageGraph(graphData) {
 		datasets: [
 			{
 				label: "Wether",
-				fillColor: "rgba(220,220,220,0.2)",
-				strokeColor: "rgba(220,220,220,1)",
-				pointColor: "rgba(220,220,220,1)",
+				fillColor: "rgba(209,43,97,0.2)",
+				strokeColor: "rgba(245,53,115,1)",
+				pointColor: "rgba(245,53,115,1)",
 				pointStrokeColor: "#fff",
 				pointHighlightFill: "#fff",
 				pointHighlightStroke: "rgba(220,220,220,1)",
@@ -14,9 +14,9 @@ function drawUsageGraph(graphData) {
 			},
 			{
 				label: "Power Usage",
-				fillColor: "rgba(151,187,205,0.2)",
-				strokeColor: "rgba(151,187,205,1)",
-				pointColor: "rgba(151,187,205,1)",
+				fillColor: "rgba(141,12,194,0.2)",
+				strokeColor: "rgba(141,12,194,1)",
+				pointColor: "rgba(141,12,194,1)",
 				pointStrokeColor: "#fff",
 				pointHighlightFill: "#fff",
 				pointHighlightStroke: "rgba(151,187,205,1)",
